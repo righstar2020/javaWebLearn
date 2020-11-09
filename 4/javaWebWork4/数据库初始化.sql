27e354bfc1547c7e1c59b1f580f9e657b8d3dcc3
rightstar2020 <1145133484@qq.com>
2020-11-09 22:19:53 +0800

    mysql数据库初始化文件

---
+++ 4/javaWebWork4/数据库初始化.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS javaweb;
+CREATE DATABASE javaweb;
+use javaweb;
+create table user_table
+(
+    id int not null auto_increment,
+    name varchar(20) unique not null,
+    password varchar(255) not null,
+    email varchar(20),
+    phone varchar(20),
+    address varchar(255),
+    primary key(id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
